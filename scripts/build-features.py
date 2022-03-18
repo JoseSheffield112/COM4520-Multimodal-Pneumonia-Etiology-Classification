3ceba2b663a0d54a1254856a4bb85152b77b256b
Daniel Gray <dgray4@sheffield.ac.uk>
2022-03-18 18:56:28 +0000

    Add low memory option

--- scripts/build-features.py
+++ scripts/build-features.py
@@ -3,25 +3,34 @@ from importlib import import_module
 import numpy as np
 
 save_npz = True
-feature_root = 'scripts/features'
+low_memory = False
+feature_script_root = 'scripts/features'
+feature_root = 'features'
 output_root = 'output'
 
 
 features = {}
 
-for filename in listdir(feature_root):
+for filename in listdir(feature_script_root):
     if filename.endswith('.py'):
         file = filename[:-3] # No extension
         print('Running ', file)
         feature = import_module('.' + file, 'features').main()
-        if save_npz:
+        if save_npz and not low_memory:
             features[file] = feature
         else:
             features[file] = None
 
 if save_npz:
     print('Saving compiled output...')
-    np.savez(output_root + '/features.npz', **features)
+    if low_memory:
+        for filename in listdir(feature_root):
+            if filename.endswith('.npy'):
+                file = filename[:-4] # No extension
+                features[file] = np.load(feature_root + '/' + filename)
+                
+    else:
+        np.savez(output_root + '/features.npz', **features)
     print('Saved compiled output!\n')
 
 print('Features:', ', '.join(features.keys()))
