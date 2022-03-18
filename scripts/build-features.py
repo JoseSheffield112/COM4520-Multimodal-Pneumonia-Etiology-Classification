41da3738558f705e841f3a185fec40cd31885c21
Daniel Gray <dgray4@sheffield.ac.uk>
2022-03-18 18:35:43 +0000

    Add npz option to build-features

--- scripts/build-features.py
+++ scripts/build-features.py
@@ -1,9 +1,27 @@
 from os import listdir
 from importlib import import_module
+import numpy as np
 
+save_npz = True
 feature_root = 'scripts/features'
+output_root = 'output'
+
+
+features = {}
 
 for filename in listdir(feature_root):
     if filename.endswith('.py'):
-        print('Running ', filename)
-        import_module('.' + filename[:-3], 'features').main()
+        file = filename[:-3] # No extension
+        print('Running ', file)
+        feature = import_module('.' + file, 'features').main()
+        if save_npz:
+            features[file] = feature
+        else:
+            features[file] = None
+
+if save_npz:
+    print('Saving compiled output...')
+    np.savez(output_root + '/features.npz', **features)
+    print('Saved compiled output!\n')
+
+print('Features:', ', '.join(features.keys()))
