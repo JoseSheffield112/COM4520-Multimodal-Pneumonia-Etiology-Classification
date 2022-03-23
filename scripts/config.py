e34727e1854bac75d15670b9420d5aeaa0589dab
Daniel Gray <dgray4@sheffield.ac.uk>
2022-03-23 16:23:44 +0000

    Add save_intermadiate config option

--- scripts/config.py
+++ scripts/config.py
@@ -3,14 +3,16 @@
 
 save_npz = True
 low_memory = False
-in_data_root = 'datasets/mimic-iv/mimic-iv-1.0'
-out_data_root = 'datasets/mimic-iv/mimic-iv-new'
+save_intermediates = True
+in_data_root = 'datasets/mimic-iv/mimic-iv-full-cohort-v2'
+out_data_root = 'datasets/mimic-iv/mimic-iv-full-cohort-v3'
 cohort_root = 'cohorts/data_oscar_cohort'
-origin_root = 'datasets/mimic-iv/mimic-iv-full-cohort'
+origin_root = 'datasets/mimic-iv/mimic-iv-full-cohort-v2'
 feature_script_root = 'scripts/features'
 intermediate_root = 'intermediates/'
 feature_root = 'features'
 output_root = 'output'
+
 
 #Sql databse connection data 
 #Path to json file that contains information needed to connect to a postgresql database
