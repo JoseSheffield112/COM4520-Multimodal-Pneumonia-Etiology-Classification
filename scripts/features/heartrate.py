2f04844f82190de25def83de5f3b63c5d59af2ba
Daniel Gray <dgray4@sheffield.ac.uk>
2022-03-23 16:24:40 +0000

    Refactor heartrates and temperatures to return a dataframe

--- scripts/features/heartrate.py
+++ scripts/features/heartrate.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.append(os.getcwd()) # Append current directory to sys.path. Makes it easier to run this script individually from the terminal.
+
 import numpy as np
 import pandas as pd
 from pathlib import Path
@@ -7,42 +11,43 @@ itemid_filter = [211, 220045]
 
 def main():
     print('Reading chartevents...')
-    columns = ['subject_id', 'stay_id', 'charttime', 'itemid', 'value']
+    columns = ['hadm_id', 'charttime', 'itemid', 'value']
     iter_csv = pd.read_csv(origin_root + '/icu/chartevents.csv', header=0, index_col=[0], iterator=True, chunksize=10000, usecols=columns)
     data = pd.concat([chunk[chunk.itemid.isin(itemid_filter)] for chunk in iter_csv])
 
-    print('Processing heartrateevents...')
+    print('Processing heartrates...')
     data = data.drop(columns='itemid')
-    data = pd.concat([process_patient(chunk) for chunk in [data[data.index == subject] for subject in data.index.unique()]])
+    data = pd.concat([process_admission(chunk) for chunk in [data[data.index == admission] for admission in data.index.unique()]])
     
-    print('Saving...')
-    intermediate_path = Path(intermediate_root + '/heartrateevents.csv')  
-    intermediate_path.parent.mkdir(parents=True, exist_ok=True) 
-    data.to_csv(intermediate_path)
-    print('Saved heartrateevents!')
-    print('Generating npy...')
-    heartrates = np.empty((0,24), float)
-    print('Patient count: ', len(set(data.index.values)))
-    for subject in set(data.index.values):
-        arr = np.empty(24, float)
-        chunk = [data[data.index == subject]]
-        for row in chunk:
-            arr[row.hour] = row.value
-        heartrates = np.append(heartrates, np.array([arr]), axis=0)
-    print('Saving heartrates feature...')
-    np.save(feature_root + '/heartrate.npy', heartrates)
-    print('Shape: ', heartrates.shape)
-    print('Saved heartrates!\n')
-    return heartrates
+    if save_intermediates:
+        print('Saving intermediate...')
+        intermediate_path = Path(intermediate_root + '/heartrates.csv')  
+        intermediate_path.parent.mkdir(parents=True, exist_ok=True) 
+        data.to_csv(intermediate_path)
+        print('Saved intermediate heartrates!')
 
-def process_patient(chunk):
+    print('Admission count: ', len(set(data.index.values)))
+    data.to_pickle(feature_root + '/heartrate.pickle')
+    print('Shape: ', data.shape)
+    return data
+
+def process_admission(chunk):
     first = chunk.head(1)
-    chunk = chunk[chunk.stay_id == first.stay_id.iloc[0]]
+    first = first.drop(columns='value')
+
     chunk['hour'] = (((pd.to_datetime(chunk.charttime) - pd.to_datetime(first.charttime)).dt.total_seconds()) / 3600).round(0).astype(int)
-    chunk = chunk.drop(columns=['stay_id', 'charttime'])
+    chunk = chunk.drop(columns='charttime')
     chunk = chunk[~chunk.hour.duplicated(keep='first')]
     chunk = chunk[(chunk.hour < 24) & (chunk.value.astype(float) > 0) & (chunk.value.astype(float) < 300)]
-    return chunk
+    
+    heartrates = np.empty(24, float)
+    for _, row in chunk.iterrows():
+        heartrates[int(row.hour)] = row.value
+    first = first.drop(columns='charttime')
+    first['heartrates'] = [heartrates]
+    return first
 
 if __name__ == '__main__':
-    main()
+    print('Saving heartrates feature...')
+    main().to_pickle(feature_root + '/heartrates.pickle')
+    print('Saved heartrates!\n')
--- scripts/features/temperature.py
+++ scripts/features/temperature.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.append(os.getcwd()) # Append current directory to sys.path. Makes it easier to run this script individually from the terminal.
+
 import numpy as np
 import pandas as pd
 from pathlib import Path
@@ -7,48 +11,48 @@ itemid_filter = [676, 678, 223761, 223762]
 
 def main():
     print('Reading chartevents...')
-    columns = ['subject_id', 'stay_id', 'charttime', 'itemid', 'value']
+    columns = ['hadm_id', 'charttime', 'itemid', 'value']
     iter_csv = pd.read_csv(origin_root + '/icu/chartevents.csv', header=0, index_col=[0], iterator=True, chunksize=10000, usecols=columns)
     data = pd.concat([chunk[chunk.itemid.isin(itemid_filter)] for chunk in iter_csv])
 
-    print('Processing temperatureevents...')
-    data = pd.concat([process_patient(chunk) for chunk in [data[data.index == subject] for subject in data.index.unique()]])
-
-    print('Converting temperatures...')
-    centigrades = data[data.itemid.isin([676, 223762])]
-    fahrenheits = data[data.itemid.isin([678, 223761])]
-    fahrenheits.value = ((fahrenheits.value.astype(float) - 32) * 5 / 9).round(1)
-    data = pd.concat([centigrades, fahrenheits])
-    data = data.drop(columns='itemid')
+    print('Processing temperatures...')
+    data = pd.concat([process_admission(chunk) for chunk in [data[data.index == admission] for admission in data.index.unique()]])
     
-    print('Saving...')
-    intermediate_path = Path(intermediate_root + '/temperatureevents.csv')  
-    intermediate_path.parent.mkdir(parents=True, exist_ok=True) 
-    data.to_csv(intermediate_path)
-    print('Saved temperatureevents!')
-    print('Generating npy...')
-    temperatures = np.empty((0,24), float)
-    print('Patient count: ', len(set(data.index.values)))
-    for subject in set(data.index.values):
-        arr = np.empty(24, float)
-        chunk = [data[data.index == subject]]
-        for row in chunk:
-            arr[row.hour] = row.value
-        temperatures = np.append(temperatures, np.array([arr]), axis=0)
-    print('Saving temperatures feature...')
-    np.save(feature_root + '/temperature.npy', temperatures)
-    print('Shape: ', temperatures.shape)
-    print('Saved temperatures!\n')
-    return temperatures
+    if save_intermediates:
+        print('Saving intermediate...')
+        intermediate_path = Path(intermediate_root + '/temperatures.csv')  
+        intermediate_path.parent.mkdir(parents=True, exist_ok=True) 
+        data.to_csv(intermediate_path)
+        print('Saved intermediate temperatures!')
 
-def process_patient(chunk):
+    print('Patient count: ', len(set(data.index.values)))
+    print('Shape: ', data.shape)
+    return data
+
+def process_admission(chunk):
     first = chunk.head(1)
-    chunk = chunk[chunk.stay_id == first.stay_id.iloc[0]]
+    first = first.drop(columns='value')
+
     chunk['hour'] = (((pd.to_datetime(chunk.charttime) - pd.to_datetime(first.charttime)).dt.total_seconds()) / 3600).round(0).astype(int)
-    chunk = chunk.drop(columns=['stay_id', 'charttime'])
+    chunk = chunk.drop(columns='charttime')
     chunk = chunk[~chunk.hour.duplicated(keep='first')]
+
+    centigrades = chunk[chunk.itemid.isin([676, 223762])]
+    fahrenheits = chunk[chunk.itemid.isin([678, 223761])]
+    fahrenheits.value = ((fahrenheits.value.astype(float) - 32) * 5 / 9).round(1)
+    chunk = pd.concat([centigrades, fahrenheits])
+    chunk = chunk.drop(columns='itemid')
+    
     chunk = chunk[(chunk.hour < 24) & (chunk.value.astype(float) > 25) & (chunk.value.astype(float) < 50)]
-    return chunk
+    
+    temperatures = np.empty(24, float)
+    for _, row in chunk.iterrows():
+        temperatures[int(row.hour)] = row.value
+    first = first.drop(columns='charttime')
+    first['temperatures'] = [temperatures]
+    return first
 
 if __name__ == '__main__':
-    main()
+    print('Saving temperatures feature...')
+    main().to_pickle(feature_root + '/temperatures.npy')
+    print('Saved temperatures!\n')
