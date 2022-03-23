92800c2a7378c31f0836ce6b2f3107a4389ddc38
Jose <jalves1@sheffield.ac.uk>
2022-03-23 23:31:42 +0000

    Preprocessing finished?

--- scripts/features/whitebloodcells.py
+++ scripts/features/whitebloodcells.py
@@ -1,75 +1,68 @@
 ## @NOTICE@ | @NOTICE@ | @NOTICE@ | @NOTICE@ | @NOTICE@ | @NOTICE@ | @NOTICE@ | @NOTICE@ | @NOTICE@ | @NOTICE@ | @NOTICE@ | @NOTICE@ ##
-## This script cannot handled missing hadm_id entries
+## This script cannot handled missing hadm_id entries - which mimic-iv-full-cohort-v2 has
+import os
+import sys
+sys.path.append(os.getcwd()) # Append current directory to sys.path. Makes it easier to run this script individually from the terminal.
+
 import numpy as np
 import pandas as pd
 from pathlib import Path
-from config import *
+from scripts.config import *
 
 itemid_filter = [51300, 51301]
 
 def main():
     print('Reading labeveents...')
-    columns = ['subject_id', 'hadm_id', 'charttime', 'itemid', 'value']
+    columns = ['hadm_id', 'charttime', 'itemid', 'value']
     iter_csv = pd.read_csv(origin_root + '/hosp/labevents.csv', header=0, index_col=[0], iterator=True, chunksize=1000, usecols=columns)
     data = pd.concat([chunk[chunk.itemid.isin(itemid_filter)] for chunk in iter_csv])
 
     print('Processing whitebloodcells...')
-    data = data.drop(columns=['itemid'])
-    data = pd.concat([process_patient(chunk) for chunk in [data[data.index == subject] for subject in data.index.unique()]])
+    data = pd.concat([process_admissions(chunk) for chunk in [data[data.index == admission] for admission in data.index.unique()]])
 
     ## Intermediate results
-    print('Saving...')
-    intermediate_path = Path(intermediate_root + '/whitebloodcells.csv')  
-    intermediate_path.parent.mkdir(parents=True, exist_ok=True) 
-    data.to_csv(intermediate_path)
-    print('Saved whitebloodcells!')
+    if save_intermediates:
+        intermediate_path = Path(intermediate_root + '/whitebloodcells.csv')  
+        intermediate_path.parent.mkdir(parents=True, exist_ok=True) 
+        data.to_csv(intermediate_path)
+        print('Saved whitebloodcells!')
 
-    ## main code
-    print('Generating npy...')
     print('Patient count: ', len(set(data.index.values)))
-    data = data.drop(columns=['hour'])
-    # making array here
+    print('Shape: ', data.shape)
     
-    wbc = np.empty((0,2), float)
-    for subject in set(data.index.values):
-        temp = np.empty((2), float)
-        chunk = [data[data.index == subject]]
-        #######################################
-        admission = chunk[0].hadm_id.iloc[0]
-        print(admission)
-        temp[0] = admission
-        # loading each value in respective variable
-        mean = chunk[0].value.iloc[0]
-        minimum = chunk[0].Minimum.iloc[0]
-        maximum = chunk[0].Maximum.iloc[0]
-    return wbc
+    return data
 
-def process_patient(chunk):
+def process_admissions(chunk):
     chunk.sort_values(by=['charttime'], ascending = True, inplace=True) # pandas doesn't like this, but i don't like the fact it doesn't like it without offering me an alternative so left it here
     first = chunk.head(1)
-    chunk = chunk[chunk.hadm_id == first.hadm_id.iloc[0]]
     chunk['hour'] = (((pd.to_datetime(chunk.charttime) - pd.to_datetime(first.charttime)).dt.total_seconds()) / 3600).round(0).astype(int)
-    chunk = chunk.drop(columns=['charttime'])
+
+    first = first.drop(columns=['itemid', 'value', 'charttime'])
+    chunk = chunk.drop(columns=['itemid', 'charttime'])
+    
     chunk = chunk[~chunk.hour.duplicated(keep='first')]
-    chunk = chunk[(chunk.hour < 24) & (chunk.value.astype(float) > 0) & (chunk.value.astype(float) < 1000)] # in SQL anything above 11 is abnormal - there's actually a patient with 281!
-    ##
+    
+    ## Calculating min/max/mean
     values = (chunk.size)/3 # gives me (cols*rows), so divided by 3 since theres still subject id, value, hour
-    max = min = chunk.value.iloc[0].astype(float)
+    maximum = minimum = mean = chunk.value.iloc[0].astype(float)
     if(values>1): #if more than 1 record
         sum=0.0
         for i in range(0,int(values)): # we iterate over them and get mean
             value = chunk.value.iloc[i].astype(float)
-            if(value<min):
-                min = value
-            elif(value>max):
-                max = value
+            if(value<minimum):
+                minimum = value
+            elif(value>maximum):
+                maximum = value
             sum=sum+value
         mean = sum/values
         chunk.value.iloc[0] = mean
-    chunk['Minimum'] = min
-    chunk['Maximum'] = max
-    chunk = chunk.head(1) # dropping other rows
-    return chunk
+
+    ## Final array
+    temp = np.array([minimum, maximum, mean])
+    first['wbcount'] = [temp]
+    return first
 
 if __name__ == '__main__':
-    main()
+    print('Saving temperatures feature...')
+    main().to_pickle(feature_root + '/temperatures.pickle')
+    print('Saved temperatures!\n')
