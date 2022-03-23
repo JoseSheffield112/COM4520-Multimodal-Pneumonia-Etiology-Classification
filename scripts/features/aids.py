c22884047c0331faa46cff16d3ebf5e5cded0403
Daniel Gray <dgray4@sheffield.ac.uk>
2022-03-23 18:42:10 +0000

    Update aids feature to return dataframe

--- scripts/features/aids.py
+++ scripts/features/aids.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.append(os.getcwd()) # Append current directory to sys.path. Makes it easier to run this script individually from the terminal.
+
 import numpy as np
 import pandas as pd
 from pathlib import Path
@@ -7,34 +11,37 @@ icd_filter = ['042', '043', '044', '0449']
 
 def main():
     print('Reading diagnoses_icd...')
-    columns = ['subject_id', 'icd_code']
-    iter_csv = pd.read_csv(origin_root + '/hosp/diagnoses_icd.csv', header=0, index_col=[0], iterator=True, chunksize=1000, usecols=columns)
+    columns = ['subject_id', 'hadm_id', 'icd_code']
+    iter_csv = pd.read_csv(origin_root + '/hosp/diagnoses_icd.csv', header=0, iterator=True, chunksize=1000, usecols=columns)
     data = pd.concat([chunk for chunk in iter_csv])
 
-    print('Processing aidsresults...')
-    data = pd.concat([process_patient(chunk) for chunk in [data[data.index == subject] for subject in data.index.unique()]])
+    print('Processing aids...')
+    data = pd.concat([process_patient(chunk) for chunk in [data[data.subject_id == subject] for subject in data.subject_id.unique()]])
 
-    print('Saving...')
-    intermediate_path = Path(intermediate_root + '/aidsresults.csv')  
-    intermediate_path.parent.mkdir(parents=True, exist_ok=True) 
-    data.to_csv(intermediate_path)
-    print('Saved aidsresults!')
-    print('Generating npy...')
-    aids = np.empty((0), float)
-    print('Patient count: ', len(set(data.index.values)))
-    aids = data.values.astype(int)
-    print('Saving aids feature...')
-    np.save(feature_root + '/aids.npy', aids)
-    print('Shape: ', aids.shape)
+    if save_intermediates:
+        print('Saving intermediate...')
+        intermediate_path = Path(intermediate_root + '/aids.csv')  
+        intermediate_path.parent.mkdir(parents=True, exist_ok=True) 
+        data.to_csv(intermediate_path)
+        print('Saved intermediate aids!')
+
+    print('Admission count: ', len(set(data.index.values)))
+    print('Shape: ', data.shape)
     print('Saved aids!\n')
-    return aids
+    return data
 
 def process_patient(chunk):
-    chunk['has_aids'] = chunk.isin(icd_filter)
-    chunk = chunk.drop(columns='icd_code')
-    chunk = chunk[~chunk.has_aids.duplicated(keep='first')]
-    if True in chunk.values:
-        chunk = chunk[chunk.has_aids == True]
+    chunk['has_aids'] = chunk.icd_code.isin(icd_filter).astype(int)
+    chunk = chunk.drop(columns=['subject_id', 'icd_code'])
+    chunk = chunk.drop_duplicates(['hadm_id', 'has_aids'])
+    if chunk.values.any():
+        id = chunk.has_aids.idxmax()
+        before = chunk.iloc[:id-1, :]
+        after = chunk.iloc[id-1:, :]
+        after = after.assign(has_aids=1)
+        chunk = pd.concat([before, after])
+
+    chunk = chunk.set_index('hadm_id')
     return chunk
 
 if __name__ == '__main__':
