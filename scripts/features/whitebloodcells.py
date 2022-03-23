1bf523b13845f391578928d2ef7a0f4c19de5b45
Jose <jalves1@sheffield.ac.uk>
2022-03-23 18:51:26 +0000

    added min & max cols
    Also had to change upper bound - there's a value with 291! absurdly abnormal!

--- scripts/features/whitebloodcells.py
+++ scripts/features/whitebloodcells.py
@@ -1,9 +1,6 @@
-## TODO list:
+    ## TODO list:
 # Need to drop everything else but the values
 # Need to tie values to a hadm_id
-
-
-#print("-x--x--x--x--x--x--x--x--x--x--x-")
 
 ## @NOTICE@ | @NOTICE@ | @NOTICE@ | @NOTICE@ | @NOTICE@ | @NOTICE@ | @NOTICE@ | @NOTICE@ | @NOTICE@ | @NOTICE@ | @NOTICE@ | @NOTICE@ ##
 ## This script cannot handled missing hadm_id entries
@@ -30,24 +27,10 @@ def main():
     data.to_csv(intermediate_path)
     print('Saved whitebloodcells!')
     print('Generating npy...')
-    wbc = np.empty((0,2), float)
+    wbc = np.empty((0), float)
     print('Patient count: ', len(set(data.index.values)))
     data = data.drop(columns=['hour'])
-    # making array here
-    for subject in set(data.index.values):
-            arr = np.empty(2, float)
-            chunk = [data[data.index == subject]]
-            print("chunk:\n")
-            print(chunk)
-            print(chunk[0].value.iloc[0])
-            print(chunk[0].hadm_id.iloc[0])
-            # for row in chunk:
-            #     print("row: \n")
-            #     print(row.value.iloc[0])
-            #     print(row.hadm_id.iloc[0])
-            wbc = np.append(wbc, np.array([arr]), axis=0)
-    # final construction
-    #wbc = data.values.astype(int)
+    wbc = data.values.astype(int)
     print('Saving white blood cell values...')
     np.save(feature_root + '/wbc.npy', wbc)
     print('Shape: ', wbc.shape)
@@ -61,16 +44,24 @@ def process_patient(chunk):
     chunk['hour'] = (((pd.to_datetime(chunk.charttime) - pd.to_datetime(first.charttime)).dt.total_seconds()) / 3600).round(0).astype(int)
     chunk = chunk.drop(columns=['charttime']) # TODO - need to remove the subject ID & hour so we only keep WBC for each hadm
     chunk = chunk[~chunk.hour.duplicated(keep='first')]
-    chunk = chunk[(chunk.hour < 24) & (chunk.value.astype(float) > 0) & (chunk.value.astype(float) < 100)] # in SQL anything above 11 is abnormal
+    chunk = chunk[(chunk.hour < 24) & (chunk.value.astype(float) > 0) & (chunk.value.astype(float) < 1000)] # in SQL anything above 11 is abnormal - there's actually a patient with 281!
+    ##
     values = (chunk.size)/3 # gives me (cols*rows), so divided by 3 since theres still subject id, value, hour
+    max = min = chunk.value.iloc[0].astype(float)
     if(values>1): #if more than 1 record
         sum=0.0
         for i in range(0,int(values)): # we iterate over them and get mean
-            sum=sum+(chunk.value.iloc[i].astype(float))
+            value = chunk.value.iloc[i].astype(float)
+            if(value<min):
+                min = value
+            elif(value>max):
+                max = value
+            sum=sum+value
         mean = sum/values
         chunk.value.iloc[0] = mean
+    chunk['min'] = min
+    chunk['max'] = max
     chunk = chunk.head(1) # dropping other rows
-    print(chunk)
     return chunk
 
 if __name__ == '__main__':
