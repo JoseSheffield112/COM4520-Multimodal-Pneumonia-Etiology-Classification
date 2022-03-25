7fb88111c43829c0529c53cf63d78ce8ca1db8f2
Daniel Gray <dgray4@sheffield.ac.uk>
2022-03-25 19:22:11 +0000

    Incomplete impk implementation

--- scripts/build-features.py
+++ scripts/build-features.py
@@ -50,7 +50,18 @@ def preprocess(table):
             print('failed:', e)
     print()
     return table
-            
+
+def format_timeseries(table, columns):
+    table = table.drop(columns=[col for col in list(table) if col not in columns])
+    arr = table.to_numpy()
+    arr = np.array([row.tolist() for row in arr.flatten()]).reshape(arr.shape[0], -1, arr.shape[1])
+    return arr
+
+def format_static(table, columns):
+    table = table.drop(columns=[col for col in list(table) if col not in columns])
+    arr = table.to_numpy()
+    arr = np.array([np.hstack(row) for row in arr])
+    return arr
 
 if __name__=='__main__':
     features = [feature for feature in get_individual_features() if type(feature) is type(pd.DataFrame())]
@@ -67,28 +78,52 @@ if __name__=='__main__':
     print('Saved data csv!')
 
     print('Creating test...')
-    test_labels = pd.read_csv(labels_root + '/test.csv', header=0, index_col=[0], usecols=['hadm_id', 'etiology'])
-    test = pd.merge(features, test_labels, left_index=True, right_index=True)
+    test_set = pd.read_csv(labels_root + '/test.csv', header=0, index_col=[0], usecols=['hadm_id', 'etiology'])
+    test_table = pd.merge(features, test_set, left_index=True, right_index=True)
+    test_ts = format_timeseries(test_table, ['heartrates', 'systolic_blood_pressure', 'temperatures'])
+    print('Timeseries shape:', test_ts.shape)
+    test_static = format_static(test_table, ['aids', 'mscancer', 'whitebloodcells'])
+    print('Static shape:', test_static.shape)
+    etiologies = test_table.etiology.values
+    test_labels = etiologies.reshape(etiologies.shape[0], 1)
+    print(type(test_labels), test_labels.shape)
+    print('Labels shape:', test_labels.shape)
     print('Saving test csv...')
     csv_path = Path(output_root + '/test.csv')  
     csv_path.parent.mkdir(parents=True, exist_ok=True)
-    test.to_csv(csv_path)
+    test_table.to_csv(csv_path)
     print('Saved test csv!')
 
     print('Creating train...')
-    train_labels = pd.read_csv(labels_root + '/train.csv', header=0, index_col=[0], usecols=['hadm_id', 'etiology'])
-    train = pd.merge(features, train_labels, left_index=True, right_index=True)
+    train_set = pd.read_csv(labels_root + '/train.csv', header=0, index_col=[0], usecols=['hadm_id', 'etiology'])
+    train_table = pd.merge(features, train_set, left_index=True, right_index=True)
+    train_ts = format_timeseries(train_table, ['heartrates', 'systolic_blood_pressure', 'temperatures'])
+    print('Timeseries shape:', train_ts.shape)
+    train_static = format_static(train_table, ['aids', 'mscancer', 'whitebloodcells'])
+    print('Static shape:', train_static.shape)
+    etiologies = train_table.etiology.values
+    train_labels = etiologies.reshape(etiologies.shape[0], 1)
+    print('Labels shape:', train_labels.shape)
     print('Saving train csv...')
     csv_path = Path(output_root + '/train.csv')  
     csv_path.parent.mkdir(parents=True, exist_ok=True)
-    train.to_csv(csv_path)
+    train_table.to_csv(csv_path)
     print('Saved train csv!')
-
 
     if save_npz:
         print('Saving output im.npz...')
-        test_array = test.to_numpy()
-        train_array = train.to_numpy()
+        test_array = {
+            'timeseries': test_ts,
+            'static': test_static,
+            'labels': test_labels
+        }
+        print('Test shapes:', *[test_array[arr].shape for arr in test_array])
+        train_array = {
+            'timeseries': train_ts,
+            'static': train_static,
+            'labels': train_labels
+        }
+        print('Train shapes:', *[train_array[arr].shape for arr in train_array])
 
         impk_path = Path(output_root + '/im.npz')  
         impk_path.parent.mkdir(parents=True, exist_ok=True)
@@ -96,5 +131,5 @@ if __name__=='__main__':
         print('Saved output im.npz!')
 
         impk = np.load(impk_path, allow_pickle=True)
-        print('\nFirst test', impk['test'][0], sep='\n')
-        print('\nFirst train', impk['train'][0], sep='\n')
+        print('\nFirst test', impk['test'].timeseries, sep='\n')
+        print('\nFirst train', [arr[:5] for arr in impk['train']], sep='\n')
