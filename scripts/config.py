5dd1500e58a4852c9bbb17e5e464521cfa36c6c9
Daniel Gray <dgray4@sheffield.ac.uk>
2022-03-25 12:02:14 +0000

    Separate output into test and train

--- scripts/config.py
+++ scripts/config.py
@@ -4,14 +4,16 @@
 save_npz = True
 low_memory = False
 save_intermediates = True
-overwrite_cache = True
+overwrite_cache = False
 in_data_root = 'datasets/mimic-iv/mimic-iv-full-cohort-v2'
 out_data_root = 'datasets/mimic-iv/mimic-iv-full-cohort-v3'
 cohort_root = 'cohorts/data_oscar_cohort'
 origin_root = 'datasets/mimic-iv/mimic-iv-full-cohort-v2'
 feature_script_root = 'scripts/features'
+preprocessing_script_root = 'scripts/preprocessing'
 intermediate_root = 'intermediates/'
 feature_root = 'features'
+labels_root = 'cohorts/data_oscar_cohort'
 output_root = 'output'
 
 
