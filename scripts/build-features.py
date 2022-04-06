d7ad8513bbdb3c64017ad06e8f1e8a2d589a97a4
Daniel Gray <dgray4@sheffield.ac.uk>
2022-04-06 16:04:22 +0100

    Add cohort array to im.pk

--- scripts/build-features.py
+++ scripts/build-features.py
@@ -91,7 +91,10 @@ def get_image_data():
     test_images = [(sample['hadm_id'].item(),sample['img'].detach().cpu().numpy()) for sample in test_images]
     test_images = pd.DataFrame(test_images, columns = ['hadm_id','image']).set_index('hadm_id')
 
-    return {'train':train_images,'valid':valid_images,'test':test_images}
+    cohort_images = pd.concat([train_images, test_images, valid_images])
+    dfs = {'cohort':cohort_images,'train':train_images,'valid':valid_images,'test':test_images}
+    print(*[key + '_images shape: ' + str(dfs[key].shape) for key in dfs], sep='\n')
+    return dfs
 
 
 
@@ -115,6 +118,7 @@ if __name__=='__main__':
 
     print('Creating test...')
     test_set = pd.read_csv(labels_root + '/test.csv', header=0, index_col=[0], usecols=['hadm_id', 'etiology']).astype({'etiology': 'int32'})
+    print('Test set shape:', test_set.shape)
     test_table = pd.merge(features, test_set, left_index=True, right_index=True)
     test_table = pd.merge(test_table, images['test'], left_index=True, right_index=True)
 
@@ -134,6 +138,7 @@ if __name__=='__main__':
 
     print('Creating train...')
     train_set = pd.read_csv(labels_root + '/train.csv', header=0, index_col=[0], usecols=['hadm_id', 'etiology']).astype({'etiology': 'int32'})
+    print('Train set shape:', train_set.shape)
     train_table = pd.merge(features, train_set, left_index=True, right_index=True)
     train_table = pd.merge(train_table, images['train'], left_index=True, right_index=True)
 
@@ -153,6 +158,7 @@ if __name__=='__main__':
 
     print('Creating valid...')
     valid_set = pd.read_csv(labels_root + '/valid.csv', header=0, index_col=[0], usecols=['hadm_id', 'etiology']).astype({'etiology': 'int32'})
+    print('Valid set shape:', valid_set.shape)
     valid_table = pd.merge(features, valid_set, left_index=True, right_index=True)
     valid_table = pd.merge(valid_table, images['valid'], left_index=True, right_index=True)
 
@@ -169,6 +175,28 @@ if __name__=='__main__':
     csv_path.parent.mkdir(parents=True, exist_ok=True)
     valid_table.to_csv(csv_path)
     print('Saved valid csv!')
+
+    # print('Creating cohort...')
+    # cohort_set = pd.read_csv(labels_root + '/data.csv', header=0, index_col=[0], usecols=['hadm_id', 'etiology']).astype({'etiology': 'int32'})
+    # print('Cohort set shape:', cohort_set.shape)
+    # cohort_table = pd.merge(features, cohort_set, left_index=True, right_index=True)
+    # print('cohort_table shape:', cohort_table.shape)
+    # cohort_table = pd.merge(cohort_table, images['cohort'], left_index=True, right_index=True)
+    # print('cohort_table shape:', cohort_table.shape)
+
+    # cohort_ts = format_timeseries(cohort_table)
+    # print('Timeseries shape:', cohort_ts.shape)
+    # cohort_static = format_static(cohort_table)
+    # print('Static shape:', cohort_static.shape)
+    # cohort_images = cohort_table.image.values
+    # print('Images shape:', cohort_images.shape)
+    # cohort_labels = cohort_table.etiology.values
+    # print('Labels shape:', cohort_labels.shape)
+    # print('Saving cohort csv...')
+    # csv_path = Path(output_root + '/cohort.csv')  
+    # csv_path.parent.mkdir(parents=True, exist_ok=True)
+    # cohort_table.to_csv(csv_path)
+    # print('Saved cohort csv!')
 
     if save_npz:
         print('Saving output im.npz...')
@@ -193,14 +221,22 @@ if __name__=='__main__':
             'labels': valid_labels
         }
         print('Valid shapes:', *[valid_array[arr].shape for arr in valid_array])
+        cohort_array = {
+            'timeseries': np.concatenate((test_ts, train_ts, valid_ts), axis=0),
+            'static': np.concatenate((test_static, train_static, valid_static), axis=0),
+            'image' : np.concatenate((test_images, train_images, valid_images), axis=0),
+            'labels': np.concatenate((test_labels, train_labels, valid_labels), axis=0),
+        }
+        print('Cohort shapes:', *[cohort_array[arr].shape for arr in cohort_array])
 
-        impk_path = Path(output_root + '/im.npz')  
-        impk_path.parent.mkdir(parents=True, exist_ok=True)
-        np.savez(impk_path, test=test_array, train=train_array)
-        pickle.dump( {'test':test_array, 'train':train_array, 'valid':valid_array}, open(output_root + '/im.pk', 'wb'))
-        print('Saved output im.npz!')
+        #impk_path = Path(output_root + '/im.npz')  
+        #impk_path.parent.mkdir(parents=True, exist_ok=True)
+        #np.savez(impk_path, test=test_array, train=train_array)
+        pickle.dump( {'cohort':cohort_array, 'test':test_array, 'train':train_array, 'valid':valid_array}, open(output_root + '/im.pk', 'wb'))
+        print('Saved output im.pk!')
 
         impk = pickle.load(open(output_root + '/im.pk', 'rb'))
-        print('\nFirst test', *[impk['test'][key][0] for key in impk['test']], sep='\n')
+        print('\nFirst cohort', *[impk['cohort'][key][0] for key in impk['cohort']], sep='\n')
+        # print('\nFirst test', *[impk['test'][key][0] for key in impk['test']], sep='\n')
         # print('\nFirst train', *[impk['train'][key][0] for key in impk['train']], sep='\n')
         # print('\nFirst valid', *[impk['valid'][key][0] for key in impk['valid']], sep='\n')
